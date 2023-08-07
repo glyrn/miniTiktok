b68b7d04904ee4095a44bceda0db162349a37f5e
gly <reen.gly@qq.com>
2023-08-07 16:25:15 +0800

    合并了用户信息和作者信息 的结构体 查找作者等于查找用户

--- service/userService.go
+++ service/userService.go
@@ -6,25 +6,18 @@ type UserService interface {
 	// 查询所有用户
 	//GetAllUser() []dao.User_dao
 
-	// 根据id查用户 -- 这里查的不止是个人信息 后续还需要拼接关注 点赞等用户信息
-	//GetUserById(id int64) dao.User_dao
-
 	// 根据姓名查用户
 	GetUserByName(name string) (dao.User_dao, error)
 
 	// 新增用户
 	Insert2User(user *dao.User_dao) bool
 
-	// 未登录状态 需要根据id 获取到user对象
+	// 未登录状态 需要根据id 获取到user的完整对象
 	GetUser_serviceById(userId int64) (User_service_final, error)
 }
 
 // 组装结构体信息
-// 这里是用于展示作者信息
-type User_service_Auth struct {
-	Id   int64  `json:"id,omitempty"` // 空则忽略
-	Name string `json:"name,omitempty"`
-}
+// 这里是用于展示作者信息 - 等同于展示个人信息
 
 // 业务user实例 最后封装
 
