ca9d92870dc11fd928f7861c23c2b7ab694421ab
Vighzhen <728502339@qq.com>
2023-08-08 13:19:10 +0800

    优化service层添加粉丝列表和关注列表

--- controller/followController.go
+++ controller/followController.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"miniTiktok/dao"
 	"miniTiktok/service"
 	"net/http"
 	"strconv"
@@ -11,6 +12,14 @@ import (
 type followResponse struct {
 	Response
 }
+
+type followResp struct {
+	Response
+	UserList []dao.User_dao `json:"user_list,omitempty"`
+}
+
+// 定义全局变量
+var fsi = service.FollowServiceImpl{}
 
 func Action(c *gin.Context) {
 
@@ -28,13 +37,92 @@ func Action(c *gin.Context) {
 		})
 		return
 	}
+
 	//调用添加功能如actionType等于2自动改为修改功能
-	service.InsertFollow(userId, toUserId, actionType)
+	fsi.InsertFollow(userId, toUserId, actionType)
 	fmt.Println("关注成功或取消成功")
 	c.JSON(http.StatusOK, followResponse{
 		Response{
 			StatusCode: 0,
 			StatusMsg:  "OK",
 		},
+	})
+}
+
+//_______________________________________________________________________________
+//下面两个因为user返回值没有写完功能没有完整，而且userid我直接写死
+
+// 关注列表请求
+func Follow(c *gin.Context) {
+	userId, err := strconv.ParseInt(c.Query("user_id"), 10, 64)
+	userId = 2
+	// 用户id解析出错。
+	if nil != err {
+		c.JSON(http.StatusOK, followResp{
+			Response: Response{
+				StatusCode: -1,
+				StatusMsg:  "用户id格式错误。",
+			},
+			UserList: nil,
+		})
+		return
+	}
+	// 正常获取关注列表
+	userList := fsi.GetFanIdOrFollowList("fan", userId)
+	if userList != nil {
+		c.JSON(http.StatusOK, followResp{
+			Response: Response{
+				StatusCode: -1,
+				StatusMsg:  "获取关注失败",
+			},
+			UserList: nil,
+		})
+		return
+	}
+	// 成功获取到关注列表
+	c.JSON(http.StatusOK, followResp{
+		Response: Response{
+			StatusCode: 0,
+			StatusMsg:  "OK",
+		},
+		UserList: userList,
+	})
+}
+
+// 粉丝列表请求
+func Follower(c *gin.Context) {
+	userId, err := strconv.ParseInt(c.Query("user_id"), 10, 64)
+	userId = 2
+	// 用户id解析出错。
+	if nil != err {
+		c.JSON(http.StatusOK, followResp{
+			Response: Response{
+				StatusCode: -1,
+				StatusMsg:  "用户id格式错误。",
+			},
+			UserList: nil,
+		})
+		return
+	}
+	// 正常获取关注列表
+	userList := fsi.GetFanIdOrFollowList("follow", userId)
+
+	if userList != nil {
+		c.JSON(http.StatusOK, followResp{
+			Response: Response{
+				StatusCode: -1,
+				StatusMsg:  "获取粉丝失败",
+			},
+			UserList: nil,
+		})
+		return
+	}
+	// 成功获取到关注列表
+	c.JSON(http.StatusOK, followResp{
+		Response: Response{
+			StatusCode: 0,
+			StatusMsg:  "OK",
+		},
+		UserList: userList,
 	})
 }
--- service/followService.go
+++ service/followService.go
@@ -1,15 +1,17 @@
 package service
 
-import "miniTiktok/dao"
+import (
+	"miniTiktok/dao"
+)
 
 type FollowService interface {
 
-	// 通过 id 修改 cancel状态
-	UpdateCanCelById(id int64, cancel int8)
-
-	// 增加数据
-	InsertFollow(follow dao.Follow)
+	// // 添加或取消关注
+	InsertFollow(userId, toUserId, account int64)
 
 	// 根据id获取粉丝和关注总数
 	GetFansDndAttention(id int64) (int64, int64)
+
+	// 获取用户粉丝列表或关注列表 fan  follow
+	GetFanIdOrFollowList(str string, userId int64) []dao.User_dao
 }
